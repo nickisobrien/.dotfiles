5ced7fcae7745da2ab25bf5dd01a10e3ec72d2c0
Nicholas O'Brien <nickisobrien@gmail.com>
2022-12-12 08:19:00 -0800

    Iterm Theme: Snazzy

--- install.sh
+++ install.sh
@@ -6,3 +6,5 @@ brew install spotify-tui
 brew install autojump
 brew install --cask maccy
 brew install --cask boop
+
+curl https://raw.githubusercontent.com/sindresorhus/iterm2-snazzy/main/Snazzy.itermcolors > Snazzy.itermcolors && open Snazzy.itermcolors
