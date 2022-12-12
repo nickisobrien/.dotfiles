55fb0abbec46abd53574dff99be2a2e7a951788c
Nicholas O'Brien <nickisobrien@gmail.com>
2022-12-12 09:07:28 -0800

    Plugin manager for tmux and nvim

--- install.sh
+++ install.sh
@@ -8,5 +8,10 @@ brew install --cask maccy
 brew install --cask boop
 
 curl https://raw.githubusercontent.com/sindresorhus/iterm2-snazzy/main/Snazzy.itermcolors > Snazzy.itermcolors && open Snazzy.itermcolors
+
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
